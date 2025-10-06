987a772662af92f7201b714e155fdc2b29c52cdc
vchaindz <vchaindz@users.noreply.github.com>
2025-10-06 17:37:31 +0000

    deploy: de25f3b437e521fcea5223ee35b16da57c5e5c60

--- jsonic_wasm.js
+++ jsonic_wasm.js
@@ -390,6 +390,25 @@ export class JsonDB {
         }
     }
     /**
+     * Clear the query result cache (useful for benchmarking)
+     * @returns {any}
+     */
+    clear_query_cache() {
+        try {
+            const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
+            wasm.jsondb_clear_query_cache(retptr, this.__wbg_ptr);
+            var r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
+            var r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
+            var r2 = getDataViewMemory0().getInt32(retptr + 4 * 2, true);
+            if (r2) {
+                throw takeObject(r1);
+            }
+            return takeObject(r0);
+        } finally {
+            wasm.__wbindgen_add_to_stack_pointer(16);
+        }
+    }
+    /**
      * List all document IDs
      * @returns {any}
      */
